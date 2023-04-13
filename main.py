fd5adfd6029e02b4645538e98ba9fe18c0069082
srutikumari <130634965+srutikumari@users.noreply.github.com>
2023-04-13 09:14:50 +0000

    i did a programm yayy

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 print("hello lal")
+print("hello lal")
+name =input(" Enter your name: ")
+time =flote(input(" Enter the current time: "))
+if(time<=12):
+  print("good morning")
+elif(time<=16):
+  print("good afternoon")
+elif(time<=20):
+  print("Good Evening")
+else:
+  print("Good Night")
